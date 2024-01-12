74fae577fc46fbc9cf60bd1c8fcd249ffc9b07e3
Hariharan K <80878386+Haran-K@users.noreply.github.com>
2024-01-12 18:39:34 +0530

    Add files via upload

---
+++ dist/run-me.bat
@@ -0,0 +1,3 @@
+Echo "Welcome to whatsapp Hack"
+
+WhatsAppAutoForward.exe --source "{{Source Group/Contact name}}" --sourceIsGroup True --target "{{Target Group/Contact Name}}" --prefix "{{Prefix for message}}"
